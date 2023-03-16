dad10c0dd1a64934624ec3b5685a8a6cd900c300
Anastasiia Sirenko <nastya9701@ukr.net>
2023-03-16 13:26:55 +0200

    logwarm_calculate.py

--- calculate.py
+++ calculate.py
@@ -4,6 +4,7 @@ logging.basicConfig(level=logging.DEBUG, filename="logfile.log")
 operation = input("Введи дію, використовуючи відповідне число: 1 Додавання, 2 Віднімання, 3 Множення, 4 Ділення:")
 num1 = int(input("Введи перше число: "))
 num2 = int(input("Введи друге число: "))
+
 operator1 = 'Додаю'
 operator2 = 'Віднімаю'
 operator3 = 'Множу'
@@ -11,20 +12,27 @@ operator4 = 'Ділю'
 
 def calculator():
     if operation == '1':
+        num3 = int(input("Введи третє число: "))
         print (operator1), 
-        result = num1 + num2
+        result = num1 + num2 + num3
     elif operation == '2':
         print (operator2)
         result = num1 - num2
     elif operation == '3':
+        num3 = int(input("Введи третє число: "))
         print (operator3)
-        result = num1 * num2
+        result = num1 * num2 * num3
     elif operation == '4':
-        print (operator4)
-        result = num1 / num2
-        return
+        if num2 == 0:
+            logging.warning(f"Друге число  це {num2}. На 0 ділити не можна.")
+            print("На 0 ділити не можна.")
+            return None
+        else:
+            print (operator4)
+            result = num1 / num2
+            return
         
     logging.info(f" {num1} i {num2} ")
-    print(f" {num1} i {num2} ")
+    print(f" {num1}, {num2} i {num3}")
     print (f"Результат: {result}")
 calculator()
